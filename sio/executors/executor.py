01d03125746fa17da3a3274f6a58fa6939a921f5
Olaf Targowski <me@otsrv.net>
2024-12-03 09:33:02 +0100

    Switch cpu-exec to real-time sio2jail

--- sio/executors/executor.py
+++ sio/executors/executor.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 from sio.executors import common, interactive_common
-from sio.workers.executors import SupervisedExecutor
+from sio.workers.executors import Sio2JailExecutor
 
 
 def run(environ):
-    return common.run(environ, SupervisedExecutor())
+    return common.run(environ, Sio2JailExecutor(measure_real_time=True))
 
 def interactive_run(environ):
-    return interactive_common.run(environ, SupervisedExecutor())
+    return interactive_common.run(environ, Sio2JailExecutor(measure_real_time=True))
